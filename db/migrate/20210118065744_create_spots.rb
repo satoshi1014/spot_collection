75dcc6eee31959f508a28d21cc6cad440ad71728
EC2 Default User <ec2-user@ip-172-31-3-104.ap-northeast-1.compute.internal>
2021-01-18 07:12:51 +0000

    spotsのマイグレーションファイルを編集

--- db/migrate/20210118065744_create_spots.rb
+++ db/migrate/20210118065744_create_spots.rb
@@ -1,7 +1,12 @@
 class CreateSpots < ActiveRecord::Migration[5.2]
   def change
     create_table :spots do |t|
-
+      t.string  :place
+      t.string  :address
+      t.string  :impression
+      t.text  :thought
+      t.float :rate
+      t.integer :user_id
       t.timestamps
     end
   end
